1e6ca6a5f59e47194bb9e492a601129661a1be0b
developer7724 <sayantand.cse.jisu21@gmail.com>
2024-04-06 10:57:16 +0530

    Cryptocurrency Website

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Binance</title>
+    <title>Index</title>
     <link rel="stylesheet" type="text/css" href="binance/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
 </head>
